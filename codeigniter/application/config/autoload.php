e4414dfac8a716ce2542e6184e34f519360f2696
賴瑞晟 <skylovely2005@hotmail.com>
2014-04-05 13:59:11 +0800

    Update CI
    Setup the DB Connect Code that can run in local enviroment.
    
    Need to import the "4_smart0eddie.sql" file to local database named
    "4_smart0eddie" first.

--- codeigniter/application/config/autoload.php
+++ codeigniter/application/config/autoload.php
@@ -52,9 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array();
-
-
+$autoload['libraries'] = array('database');
 /*
 | -------------------------------------------------------------------
 |  Auto-load Helper Files
