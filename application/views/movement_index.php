0fb18638c654720444e4b99518d4d83729ed6f7a
RedBeanPieLarry <RedBeanPieLarry@gmail.com>
2014-05-06 23:35:59 +0800

    fix a problem that about page was missing css.

--- application/views/movement_index.php
+++ application/views/movement_index.php
@@ -19,6 +19,7 @@
     <link rel = "stylesheet" href = "assets/css/movement.css">
     <link rel = "stylesheet" href = "assets/css/menu.css">
     <link rel = "stylesheet" href = "assets/css/logo.css">
+    <link rel = "stylesheet" href = "assets/css/about.css">
 </head>
 <body >
 
