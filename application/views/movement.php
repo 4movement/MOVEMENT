e132928712dec6d6bc5283a7a4ee373dfc07722e
smart0eddie <smart0eddie@gmail.com>
2014-05-16 21:57:40 +0800

    add back .wrapper in movement.php

--- application/views/movement.php
+++ application/views/movement.php
@@ -48,137 +48,139 @@
         <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
 		<div id = "movement_layout" class = "layout">
-			<div class = "movement_display_mode">
-				<span class="tag_type">
-					<li class = "tag_type time">
-						<img src="../../assets/img/movement/button/mo-ti01.png" class="none_selected"  />
-						 <img src="../../assets/img/movement/button/mo-ti02.png" class="selected"/>
-					</li>
-					<li class = "tag_type hot">
-						<img src="../../assets/img/movement/button/mo-ho01.png" class="none_selected"  />
-						<img src="../../assets/img/movement/button/mo-ho02.png" class="selected"/>
-					</li>
-					<li class = "tag_type new">
-						<img src="../../assets/img/movement/button/mo-ne01.png" class="none_selected"  />
-						<img src="../../assets/img/movement/button/mo-ne02.png" class="selected"/>
-					</li>
-					<li class = "tag_type issue">
-						<img src="../../assets/img/movement/button/mo-is01.png" class="none_selected"  />
-						<img src="../../assets/img/movement/button/mo-is02.png" class="selected"/>
-					</li>
-				</span><!--tag_type -->
-				<span class="display_type">
-					<li class="display_type picture">
-						<img src="../../assets/img/movement/button/mo-pi01.png" class="none_selected"  />
-						<img src="../../assets/img/movement/button/mo-pi02.png" class="selected" />
-					</li>
-					<li class="display_type list">
-						<img src="../../assets/img/movement/button/mo-li01.png" class="none_selected"  />
-						<img src="../../assets/img/movement/button/mo-li02.png" class="selected" />
-					</li>
-				</span><!--display_type -->
-			</div>
-			<div id = "movement_calender">
-                <ul id = "movement_month_list">
-                    <li class = "month_list" year = "2014">
-                        <div class = "circle"></div>
-                        <span> 2014.</span>
-                    </li>
-                    <li class = "month_list" year = "2014" month = "1">
-                        <div class = "circle"></div>
-                        <span>Jan.</span>
-                    </li>
-                    <li class = "month_list" year = "2014" month = "2">
-                        <div class = "circle"></div>
-                        <span>Feb.</span>
-                    </li>
-                    <li class = "month_list" year = "2014" month = "3">
-                        <div class = "circle"></div>
-                        <span>Mar.</span>
-                    </li>
-                            <li class = "month_list" year = "2014" month = "4">
-                                <div class = "circle"></div>
-                                <span>Apr.</span>
-                            </li>
-                            <li class = "month_list" year = "2014" month = "5">
-                                <div class = "circle"></div>
-                                <span>May.</span>
-                            </li>
-                            <li class = "month_list selected" year = "2014" month = "6">
-                                <div class = "circle"></div>
-                                <span>Jun.</span>
-                            </li>
-                            <li class = "month_list" year = "2014" month = "7">
-                                <div class = "circle"></div>
-                                <span>Jul.</span>
-                            </li>
-                            <li class = "month_list " year = "2014" month = "8">
-                                <div class = "circle"></div>
-                                <span>Aug.</span>
-                            </li>
-                            <li class = "month_list " year = "2014" month = "9">
-                                <div class = "circle"></div>
-                                <span>Sep.</span>
-                            </li>
-                            <li class = "month_list new" year = "2014" month = "10">
-                                <div class = "circle"></div>
-                                <span>Oct.</span>
-                            </li>
-                            <li class = "month_list" year = "2014" month = "11">
-                                <div class = "circle"></div>
-                                <span>Nov.</span>
-                            </li>
-                            <li class = "month_list" year = "2014" month = "12">
-                                <div class = "circle"></div>
-                                <span>Dec.</span>
-                            </li>
-                            <li class = "month_list" year = "2015">
-                                <div class = "circle"></div>
-                                <span> 2015.</span>
-                   </li>
-                </ul>
-             </div><!--calender-->
-			 <div id="movement_list"  class="masonry js-masonry"  data-masonry-options='{ "columnWidth": 350, "itemSelector": ".item","gutter": 20}'>       
-				<li class="item">
-					<div id="movement_list_data">
-						<div class= "mask"></div>
-						<img class="photo" src="../../assets/img/sample.jpg">
-							<div class="list_title_nav">
-								<div class="list_title">Media Monoploy</div>
-								<div class="list_nav">
-									<span class="list_date">2/14</span>
-									<span class="list_place">
-										<img src="../../assets/img/movement/button/mo-lo.png" />
-										<span>Taipei<span>
-									</span>
-									<span class="list_category"><img src="" /></span>
+			<div id="movement" class="wrapper">
+				<div class = "movement_display_mode">
+					<span class="tag_type">
+						<li class = "tag_type time">
+							<img src="../../assets/img/movement/button/mo-ti01.png" class="none_selected"  />
+							 <img src="../../assets/img/movement/button/mo-ti02.png" class="selected"/>
+						</li>
+						<li class = "tag_type hot">
+							<img src="../../assets/img/movement/button/mo-ho01.png" class="none_selected"  />
+							<img src="../../assets/img/movement/button/mo-ho02.png" class="selected"/>
+						</li>
+						<li class = "tag_type new">
+							<img src="../../assets/img/movement/button/mo-ne01.png" class="none_selected"  />
+							<img src="../../assets/img/movement/button/mo-ne02.png" class="selected"/>
+						</li>
+						<li class = "tag_type issue">
+							<img src="../../assets/img/movement/button/mo-is01.png" class="none_selected"  />
+							<img src="../../assets/img/movement/button/mo-is02.png" class="selected"/>
+						</li>
+					</span><!--tag_type -->
+					<span class="display_type">
+						<li class="display_type picture">
+							<img src="../../assets/img/movement/button/mo-pi01.png" class="none_selected"  />
+							<img src="../../assets/img/movement/button/mo-pi02.png" class="selected" />
+						</li>
+						<li class="display_type list">
+							<img src="../../assets/img/movement/button/mo-li01.png" class="none_selected"  />
+							<img src="../../assets/img/movement/button/mo-li02.png" class="selected" />
+						</li>
+					</span><!--display_type -->
+				</div>
+				<div id = "movement_calender">
+	                <ul id = "movement_month_list">
+	                    <li class = "month_list" year = "2014">
+	                        <div class = "circle"></div>
+	                        <span> 2014.</span>
+	                    </li>
+	                    <li class = "month_list" year = "2014" month = "1">
+	                        <div class = "circle"></div>
+	                        <span>Jan.</span>
+	                    </li>
+	                    <li class = "month_list" year = "2014" month = "2">
+	                        <div class = "circle"></div>
+	                        <span>Feb.</span>
+	                    </li>
+	                    <li class = "month_list" year = "2014" month = "3">
+	                        <div class = "circle"></div>
+	                        <span>Mar.</span>
+	                    </li>
+	                            <li class = "month_list" year = "2014" month = "4">
+	                                <div class = "circle"></div>
+	                                <span>Apr.</span>
+	                            </li>
+	                            <li class = "month_list" year = "2014" month = "5">
+	                                <div class = "circle"></div>
+	                                <span>May.</span>
+	                            </li>
+	                            <li class = "month_list selected" year = "2014" month = "6">
+	                                <div class = "circle"></div>
+	                                <span>Jun.</span>
+	                            </li>
+	                            <li class = "month_list" year = "2014" month = "7">
+	                                <div class = "circle"></div>
+	                                <span>Jul.</span>
+	                            </li>
+	                            <li class = "month_list " year = "2014" month = "8">
+	                                <div class = "circle"></div>
+	                                <span>Aug.</span>
+	                            </li>
+	                            <li class = "month_list " year = "2014" month = "9">
+	                                <div class = "circle"></div>
+	                                <span>Sep.</span>
+	                            </li>
+	                            <li class = "month_list new" year = "2014" month = "10">
+	                                <div class = "circle"></div>
+	                                <span>Oct.</span>
+	                            </li>
+	                            <li class = "month_list" year = "2014" month = "11">
+	                                <div class = "circle"></div>
+	                                <span>Nov.</span>
+	                            </li>
+	                            <li class = "month_list" year = "2014" month = "12">
+	                                <div class = "circle"></div>
+	                                <span>Dec.</span>
+	                            </li>
+	                            <li class = "month_list" year = "2015">
+	                                <div class = "circle"></div>
+	                                <span> 2015.</span>
+	                   </li>
+	                </ul>
+	             </div><!--calender-->
+				 <div id="movement_list"  class="masonry js-masonry"  data-masonry-options='{ "columnWidth": 350, "itemSelector": ".item","gutter": 20}'>       
+					<li class="item">
+						<div id="movement_list_data">
+							<div class= "mask"></div>
+							<img class="photo" src="../../assets/img/sample.jpg">
+								<div class="list_title_nav">
+									<div class="list_title">Media Monoploy</div>
+									<div class="list_nav">
+										<span class="list_date">2/14</span>
+										<span class="list_place">
+											<img src="../../assets/img/movement/button/mo-lo.png" />
+											<span>Taipei<span>
+										</span>
+										<span class="list_category"><img src="" /></span>
+									</div>
 								</div>
-							</div>
-						</img>
-					</div>
-				</li>
-				<li class="item">
-					<img src="../../assets/img/sample.jpg" />
-				</li>
-				<li class="item">
-					<img src="../../assets/img/sample.jpg" />
-				</li>
-				<li class="item">
-					<img src="../../assets/img/sample.jpg" />
-				</li>
-				<li class="item">
-					<img src="../../assets/img/sample.jpg" />
-				</li>
-				<li class="item">
-					<img src="../../assets/img/sample.jpg" />
-				</li>
-				<li class="item">
-					<img src="../../assets/img/sample.jpg" />
-				</li>
-				<li class="item">
-					<img src="../../assets/img/sample.jpg" />
-				</li>
-			</div><!--movement_list-->
+							</img>
+						</div>
+					</li>
+					<li class="item">
+						<img src="../../assets/img/sample.jpg" />
+					</li>
+					<li class="item">
+						<img src="../../assets/img/sample.jpg" />
+					</li>
+					<li class="item">
+						<img src="../../assets/img/sample.jpg" />
+					</li>
+					<li class="item">
+						<img src="../../assets/img/sample.jpg" />
+					</li>
+					<li class="item">
+						<img src="../../assets/img/sample.jpg" />
+					</li>
+					<li class="item">
+						<img src="../../assets/img/sample.jpg" />
+					</li>
+					<li class="item">
+						<img src="../../assets/img/sample.jpg" />
+					</li>
+				</div><!--movement_list-->
+			</div><!--movement wrapper-->
 		</div><!-- movement_layout -->
     </body>
 </html>
